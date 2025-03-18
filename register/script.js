c221d02da5742f356dac70720b3e509a21e303b9
Ermal Shala <101928596+ermalwebdev@users.noreply.github.com>
2025-03-18 22:08:00 +0100

    main

--- register/script.js
+++ register/script.js
@@ -9,7 +9,6 @@ const registerBtn = document.querySelector(".btn-register");
 const errorMsgPassword = document.querySelector(".error-message");
 
 //Preventing links
-preventDefaultLinks(links);
 
 //email validity
 registerForm(registerBtn, passwordInput, errorMsgPassword);
